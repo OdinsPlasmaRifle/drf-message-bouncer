727e8194fce367fbf0cf2151bc69a3bda7fde895
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2020-12-18 13:51:58 +0200

    Added new filter

--- src/drf_message_bouncer/filters.py
+++ src/drf_message_bouncer/filters.py
@@ -3,10 +3,28 @@ from django_filters import rest_framework as filters
 from drf_message_bouncer.models import Message
 
 
+TRUE_VALUES = ('true', 'True', True)
+FALSE_VALUES = ('false', 'False', False)
+
+
 class MessageFilterSet(filters.FilterSet):
+    broadcasted = filters.CharFilter(method='filter_broadcasted')
 
     class Meta:
         model = Message
         fields = {
             'id': ['exact',],
         }
+
+    def broadcasted(self, queryset, name, value):
+        if value in TRUE_VALUES:
+            pass
+            # queryset.filter(
+            #     message__parent_children__session=self.request.session
+            # )
+        elif value in FALSE_VALUES:
+            queryset.exclude(
+                message__parent_children__session=self.request.session
+            )
+
+        return queryset
